de28bb4db4e642747a517373fdd8a84e871feca5
unknown <mohsinsait23@gmail.com>
2020-11-16 19:04:52 +0530

    Message is being sent successfully

--- whatsapp/whatsapp_app/views.py
+++ whatsapp/whatsapp_app/views.py
@@ -4,12 +4,22 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'whatsapp_app/home.html')
 
+def whatsApp(Ph, Message):
+    import time
+    import webbrowser as web
+    import pyautogui as pg
+    Phone = "+91"+Ph
+    web.open('https://web.whatsapp.com/send?phone='+Phone+'&text='+Message)
+    time.sleep(5)
+    pg.press('enter')
+
 def SendData(request):
     if request.method == 'POST':
         Ph = request.POST['Phone']
         Message = request.POST['Message']
-        print(Ph, Message)
-        msg = 'Message was successfully sent...'
+        #print(Ph, Message)
+        whatsApp(Ph, Message)
+        msg = 'Your message was sent successfully!'
         return render(request, "whatsapp_app/home.html", {'msg': msg})
     else:
         return HttpResponse("<h1>Your message was not delivered.</h1>")
