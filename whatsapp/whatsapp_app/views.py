80fad08fdfec4b09cc70b7167c43f1898a44f405
unknown <mohsinsait23@gmail.com>
2020-11-16 15:15:42 +0530

    added SendData function in views to submit data

--- whatsapp/whatsapp_app/views.py
+++ whatsapp/whatsapp_app/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'whatsapp_app/home.html')
+
+def SendData(request):
+    if request.method == 'POST':
+        Ph = request.POST['Phone']
+        Message = request.POST['Message']
+        print(Ph, Message)
+        msg = 'Message was successfully sent...'
+        return render(request, "whatsapp_app/home.html", {'msg': msg})
+    else:
+        return HttpResponse("<h1>Your message was not delivered.</h1>")
